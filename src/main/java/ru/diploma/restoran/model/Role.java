6cf9fbd1410f5ac1cf165b48e886fe537d61744e
panfilov.d <Undertaker-81@mail.ru>
2020-10-27 12:15:50 +0500

    crate entity

---
+++ src/main/java/ru/diploma/restoran/model/Role.java
@@ -0,0 +1,6 @@
+package ru.diploma.restoran.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/ru/diploma/restoran/model/User.java
@@ -0,0 +1,10 @@
+package ru.diploma.restoran.model;
+
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private Role role;
+}
