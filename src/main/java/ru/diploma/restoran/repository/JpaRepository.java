6fbc4d3f9f0f25d0edc1064148b92ddd9a02530b
panfilov.d <Undertaker-81@mail.ru>
2020-11-09 15:52:59 +0500

    add

---
+++ src/main/java/ru/diploma/restoran/repository/JpaRepository.java
@@ -0,0 +1,8 @@
+package ru.diploma.restoran.repository;
+
+/**
+ * @author Panfilov Dmitriy
+ * 09.11.2020
+ */
+public interface JpaRepository {
+}
---
+++ src/main/java/ru/diploma/restoran/repository/Repository.java
@@ -0,0 +1,10 @@
+package ru.diploma.restoran.repository;
+
+/**
+ * @author Panfilov Dmitriy
+ * 09.11.2020
+ */
+@org.springframework.stereotype.Repository
+public class Repository {
+
+}
